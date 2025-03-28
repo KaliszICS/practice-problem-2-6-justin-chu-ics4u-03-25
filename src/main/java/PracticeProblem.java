7ced9aada822246f69f2814482e9a81190dcdcbc
Justin <justin.chu4@student.tdsb.on.ca>
2025-03-28 06:04:17 -0400

    Mostly done but it is still tweaking

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,62 +1,164 @@
+import java.util.Arrays;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	public static void bubbleSort(double[] nums) {
+	public static int[] bubbleSort(double[] nums) {
+		int swaps = 0;
+		int steps = 0;
+		boolean swapped;
 
 		for (int i = 0; i < nums.length - 1; i++) {
+			swapped = false;
 			for (int j = 0; j < nums.length - i - 1; j++) {
+				steps++;
 				if (nums[j] > nums[j + 1]) {
-
 					double temp = nums[j];
 					nums[j] = nums[j + 1];
 					nums[j + 1] = temp;
+					swaps += 3;
+
+					swapped = true;
 				}
 			}
+			if (!swapped) {
+				break;
+			}
 		}
+		return new int[]{swaps, steps};
 	}
 
-	public static void selectionSort(double[] nums) {
+	public static int[] selectionSort(double[] nums) {
+		int swaps = 0;
+		int steps = 0;
 
 		for (int i = 0; i < nums.length - 1; i++) {
-
-			int minVal = i;
-
-			for (int j = i; j < nums.length; j++) {
-				if (nums[j] < nums[minVal]) {
-
-					double temp = nums[minVal];
-					nums[minVal] = nums[j];
-					nums[j] = temp;
+			int minIndex = i;
+			for (int j = i + 1; j < nums.length; j++) {
+				steps++;
+				if (nums[j] < nums[minIndex]) {
+					minIndex = j;
 				}
 			}
+			if (minIndex != i) {
+				double temp = nums[minIndex];
+				nums[minIndex] = nums[i];
+				nums[i] = temp;
+				swaps += 3;
+			}
 		}
+		return new int[]{swaps, steps};
 	}
 
-	public static void insertionSort(double[] nums) {
+	public static int[] insertionSort(double[] nums) {
+		int swaps = 0;
+		int steps = 0;
 
 		for (int i = 1; i < nums.length; i++) {
+			double key = nums[i];
+			swaps++;
+			int j = i-1;
+			boolean comparisonMadeInLoop = false;
 
-			double k = nums[i];
-			int j = i - 1;
+			while (j >= 0) {
+				steps++;
+				comparisonMadeInLoop = true;
 
-			while (j >= 0 && nums[j] > k) {
-				nums[j + 1] = nums[j];
-				j = j - 1;
+				if (nums[j] > key) {
+					nums[j+1] = nums[j];
+					swaps++;
+					j = j - 1;
+				}
+
+				else {
+					break;
+				}
 			}
+			nums[j + 1] = key;
+			swaps++;
+		}
+		return new int[]{swaps, steps};
+	}
 
-			nums[j + 1] = k;
+	public static String leastSwaps(double[] nums) {
+		double[] bubbleNums = Arrays.copyOf(nums, nums.length);
+		double[] selectionNums = Arrays.copyOf(nums, nums.length);
+		double[] insertionNums = Arrays.copyOf(nums, nums.length);
+
+		int[] bubbleResults = bubbleSort(bubbleNums);
+		int[] selectionResults = selectionSort(selectionNums);
+		int[] insertionResults = insertionSort(insertionNums);
+
+		int bubbleSwaps = bubbleResults[0];
+		int selectionSwaps = selectionResults[0];
+		int insertionSwaps = insertionResults[0];
+
+		String bestSort = "Bubble";
+
+		int minSwaps = bubbleSwaps;
+
+		if (insertionSwaps < minSwaps) {
+			minSwaps = insertionSwaps;
+			bestSort = "Insertion";
+		}
+
+		if (selectionSwaps < minSwaps) {
+			bestSort = "Selection";
+		}
+
+		//tie breaking
+		if (bubbleSwaps <= insertionSwaps && bubbleSwaps <= selectionSwaps) {
+			return "Bubble";
+		}
+
+		else if (insertionSwaps <= selectionSwaps) {
+			return "Insertion";
+		}
+
+		else {
+			return "Selection";
 		}
 	}
 
-	public static void q4() {
-		//Write question 4 code here
-	}
+	public static String leastIterations(double[] nums) {
+		double[] bubbleNums = Arrays.copyOf(nums, nums.length);
+		double[] selectionNums = Arrays.copyOf(nums, nums.length);
+		double[] insertionNums = Arrays.copyOf(nums, nums.length);
 
-	public static void q5() {
-		//Write question 5 code here
+		int[] bubbleResults = bubbleSort(bubbleNums);
+		int[] selectionResults = selectionSort(selectionNums);
+		int[] insertionResults = insertionSort(insertionNums);
+
+		int bubbleSteps = bubbleResults[1];
+		int selectionSteps = selectionResults[1];
+		int insertionSteps = insertionResults[1];
+
+		String bestSort = "Bubble";
+		int minSteps = bubbleSteps;
+
+		if (insertionSteps < minSteps) {
+			minSteps = insertionSteps;
+			bestSort = "Insertion";
+		}
+
+		if (selectionSteps < minSteps) {
+			bestSort = "Selection";
+		}
+
+		if (bubbleSteps <= insertionSteps && bubbleSteps <= selectionSteps) {
+			return "Bubble";
+		}
+
+		else if (insertionSteps <= selectionSteps) {
+			return "Insertion";
+		}
+
+		else {
+			return "Selection";
+		}
 	}
 
 }
