9174c21dbd8e9aa945e14694aac56842f8e53334
Justin <justin.chu4@student.tdsb.on.ca>
2025-03-28 01:36:19 -0400

    Loops

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,16 +4,51 @@ public class PracticeProblem {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static void bubbleSort(double[] nums) {
+
+		for (int i = 0; i < nums.length - 1; i++) {
+			for (int j = 0; j < nums.length - i - 1; j++) {
+				if (nums[j] > nums[j + 1]) {
+
+					double temp = nums[j];
+					nums[j] = nums[j + 1];
+					nums[j + 1] = temp;
+				}
+			}
+		}
 	}
 
-	public static void q2() {
-		//Write question 2 code here
+	public static void selectionSort(double[] nums) {
+
+		for (int i = 0; i < nums.length - 1; i++) {
+
+			int minVal = i;
+
+			for (int j = i; j < nums.length; j++) {
+				if (nums[j] < nums[minVal]) {
+
+					double temp = nums[minVal];
+					nums[minVal] = nums[j];
+					nums[j] = temp;
+				}
+			}
+		}
 	}
 
-	public static void q3() {
-		//Write question 3 code here
+	public static void insertionSort(double[] nums) {
+
+		for (int i = 1; i < nums.length; i++) {
+
+			double k = nums[i];
+			int j = i - 1;
+
+			while (j >= 0 && nums[j] > k) {
+				nums[j + 1] = nums[j];
+				j = j - 1;
+			}
+
+			nums[j + 1] = k;
+		}
 	}
 
 	public static void q4() {
